ef0093a59bc5306a9df9e1e6e05eafaa0ac5e008
vsarkhel <vsarkhel@cs.odu.edu>
2019-10-29 22:02:24 -0400

    updated lab3.py

--- ece484_lab3/lab3.py
+++ ece484_lab3/lab3.py
@@ -1,130 +1,100 @@
-# This Python file uses the following encoding: utf-8
-import sys
 import mmap
 import os
 import time
 import socket
-#from PySide2.QtWidgets import QApplication
-from PySide2 import QtCore,QtGui,QtWidgets
 
-class lab3(QtWidgets.QWidget):
-    def __init__(self, parent=None):
+# HPS/FPGA Memory Addresses
+HW_REGS_BASE = 0xFC000000
+HW_REGS_SPAN = 0x04000000
+HW_REGS_MASK = HW_REGS_SPAN - 1
+ALT_LWFPGASLVS_OFST = 0xFF200000
+LEDS = 0x00000100
+hostname = socket.gethostname()    
+IPAddr = socket.gethostbyname(hostname)
+# UDP Server Info
+UDP_IP = "192.168.0.19"
+UDP_PORT = 80
 
-        #pass    # call __init__(self) of the custom base class here
-#        QtWidgets.QWidget.__init__(self,parent)
-#        quit= QtWidgets.QPushButton("Quit")
+# Convert 1 digit into 7-seg bit pattern
+numtable = {
+    "0": 0x40,
+    "1": 0x79,
+    "2": 0x24,
+    "3": 0x30,
+    "4": 0x19,
+    "5": 0x12,
+    "6": 0x02,
+    "7": 0x78,
+    "8": 0x00,
+    "9": 0x10
+}
 
-#        lcd =QtWidgets.QLCDNumber(2)
+# Convert array of 4 numtable into mmap writable string 
+def cons(bins):
+    s = ""
+    f = 0
+    for i in range(0, 4):
+        f = f | bins[i] << (i * 7)
 
-#        slider=QtWidgets.QSlider(QtCore.Qt.Horizontal)
-#        slider.setRange(0, 99)
-#        slider.setValue(0)
-#        self.connect(quit,QtCore.SIGNAL("clicked()"),
-#                    QtWidgets.qApp, QtCore.SLOT("quit()"))
-#        self.connect(slider,QtCore.SIGNAL("valueChanged(int)"),
-#                    lcd, QtCore.SLOT("display(int)"))
+    for i in range(0, 4):
+        t = f >> (i * 8)
+        s = s + chr(t & 0xFF)
+    
+    return s
 
-#        layout=QtWidgets.QVBoxLayout()
-#        layout.addWidget(quit)
-#        layout.addWidget(lcd);
-#        layout.addWidget(slider);
-#        self.setLayout(layout);
+# Open memory as file descriptor
+fd = os.open("/dev/mem", os.O_RDWR | os.O_SYNC)
 
-        # HPS/FPGA Memory Addresses
-        HW_REGS_BASE = 0xFC000000
-        HW_REGS_SPAN = 0x04000000
-        HW_REGS_MASK = HW_REGS_SPAN - 1
-        ALT_LWFPGASLVS_OFST = 0xFF200000
-        LEDS = 0x00000100
+# Check if opened sucessfully
+if fd == -1:
+    print "error opening /dev/mem!"
+    exit()
 
-        # UDP Server Info
-        UDP_IP = "192.168.1.2"
-        UDP_PORT = 80
+# Map /dev/mem to writable block of memory
+vb = mmap.mmap(
+    fd, 
+    HW_REGS_SPAN, 
+    flags=mmap.MAP_SHARED, 
+    offset=HW_REGS_BASE
+)
 
-        # Convert 1 digit into 7-seg bit pattern
-        numtable = {
-            "0": 0x40,
-            "1": 0x79,
-            "2": 0x24,
-            "3": 0x30,
-            "4": 0x19,
-            "5": 0x12,
-            "6": 0x02,
-            "7": 0x78,
-            "8": 0x00,
-            "9": 0x10
-        }
+# 7-seg base address
+pos = (ALT_LWFPGASLVS_OFST + LEDS) & HW_REGS_MASK
 
-        # Convert array of 3 numtable into mmap writable string
-        def cons(bins):
-            s = ""
-            f = 0
-            for i in range(0, 3):
-                f = f | bins[i] << (i * 7)
+# Move memory block pointer to above address
+vb.seek(pos)
+# Display 4 number on Hex0-Hex3
+def display(a, b, c, d):
+    vb.write(cons([
+		numtable[str(d)],
+        numtable[str(c)],
+        numtable[str(b)],
+        numtable[str(a)]
+    ]))
+    vb.seek(pos)
 
-            for i in range(0, 3):
-                t = f >> (i * 8)
-                s = s + chr(t & 0xFF)
+# Sanity check
+for i in range(0, 10):
+    display(i, i, i, i)
+    time.sleep(0.1)
 
-            return s
+# Reset
+display(0, 0, 0, 0)
 
-        # Open memory as file descriptor
-        fd = os.open("/dev/mem", os.O_RDWR | os.O_SYNC)
+# UDP Setup
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
 
-        # Check if opened sucessfully
-        if fd == -1:
-            print ("error opening /dev/mem!")
-            exit()
+# UDP Server Listener
+print "READY"
+while True:
+    # Receive 4 byte string (4 digit number)
+    data, addr = sock.recvfrom(4)
+    if data == "END":
+        # Cleanup
+        vb.close()
+        os.close(fd)
+        break
+    else:
+        display(data[0], data[1], data[2], data[3])
 
-        # Map /dev/mem to writable block of memory
-        vb = mmap.mmap(
-            fd,
-            HW_REGS_SPAN,
-            flags=mmap.MAP_SHARED,
-            offset=HW_REGS_BASE
-        )
-
-        # 7-seg base address
-        pos = (ALT_LWFPGASLVS_OFST + LEDS) & HW_REGS_MASK
-
-        # Move memory block pointer to above address
-        vb.seek(pos)
-
-        def display(a, b, c):
-            vb.write(cons([
-                numtable[str(c)],
-                numtable[str(b)],
-                numtable[str(a)]
-            ]))
-            vb.seek(pos)
-
-        # Sanity check
-        for i in range(0, 10):
-            display(i, i, i)
-            time.sleep(0.1)
-
-        # Reset
-        display(0, 0, 0)
-
-        # UDP Setup
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind((UDP_IP, UDP_PORT))
-
-        # UDP Server Listener
-        print ("READY")
-        while True:
-            # Receive 3 byte string (3 digit number)
-            data, addr = sock.recvfrom(3)
-            if data == "END":
-                # Cleanup
-                vb.close()
-                os.close(fd)
-                break
-            else:
-                display(data[0], data[1], data[2])
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    window = lab3()
-    window.show()
-    sys.exit(app.exec_())
